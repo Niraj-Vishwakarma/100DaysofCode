382c2f589cca962da46b613f4fefe9e0b6b45d3a
Niraj-Vishwakarma <80817063+Niraj-Vishwakarma@users.noreply.github.com>
2021-10-18 20:02:41 +0530

    Create palindrome.cpp

---
+++ Day 3/palindrome.cpp
@@ -0,0 +1,77 @@
+// hackerrank problem [ https://www.hackerrank.com/challenges/palindrome-index/problem ]
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'palindromeIndex' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int palindromeIndex(string s) {
+
+for(int i=0; i<s.size()/2; i++)
+{
+    if(s[i] != s[s.size()-1-i])
+    {
+        int n = s.size()-1-i;
+        i++;
+        if(s[i]==s[n])
+        {
+           return i-1; 
+        }else {return n;}
+    }
+}
+return -1;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        int result = palindromeIndex(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
