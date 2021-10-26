f975b0a00f30f35142b1a792f8391772a21d4735
Niraj-Vishwakarma <80817063+Niraj-Vishwakarma@users.noreply.github.com>
2021-10-26 19:03:20 +0530

    Create Cut the sticks.cpp

---
+++ Day 10/Cut the sticks.cpp
@@ -0,0 +1,21 @@
+https://www.hackerrank.com/challenges/cut-the-sticks/problem
+
+vector<int> cutTheSticks(vector<int> arr) {
+vector<int>ans;
+
+sort(arr.begin(),arr.end());
+reverse(arr.begin(), arr.end());
+
+while(!arr.empty()){
+
+ans.push_back(arr.size());
+for(int i = 0; i<arr.size();++i)
+    arr[i]-=arr[arr.size()-1];
+while(arr.back() ==0 && !arr.empty())
+    arr.pop_back();
+
+    }
+
+return ans;
+
+}
