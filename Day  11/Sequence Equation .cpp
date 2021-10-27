ca3660cd3691a0af3804701e467642e1827ff820
Niraj-Vishwakarma <80817063+Niraj-Vishwakarma@users.noreply.github.com>
2021-10-27 09:51:42 +0530

    Update Sequence Equation .cpp

--- Day  11/Sequence Equation .cpp
+++ Day  11/Sequence Equation .cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+// o[n^2]
 int main()
 {
    int n;
@@ -22,6 +23,37 @@ int main()
        ans[a[i]] = index;    
    }
   
+  for(int i=1; i<=n; i++)
+  {
+     cout<<ans[i]<<"\n"; 
+  }
+    
+    return 0;
+}
+
+
+//O[n]
+
+
+int main()
+{
+   int n;
+   cin>>n;
+   vector<int>ans(n+1, 0);
+   vector<int> a(n+1,0);
+   
+   for(int i=1; i<=n; i++)
+   {
+       int n;
+       cin>>a[i];
+   }
+   
+   for(int i=1; i<=n; i++)
+   {
+       ans [ a[a[i]] ] = i; 
+   }
+   
+   
   for(int i=1; i<=n; i++)
   {
      cout<<ans[i]<<"\n"; 
