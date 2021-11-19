fdbe7387bbb63ce2da70af72a091813068f2a1b7
Niraj-Vishwakarma <80817063+Niraj-Vishwakarma@users.noreply.github.com>
2021-11-19 09:23:43 +0530

    Create InsertionSortRecursion.cpp

---
+++ Day  30/InsertionSortRecursion.cpp
@@ -0,0 +1,23 @@
+void insertionSortRecursive(int arr[], int n)
+{
+    // Base case
+    if (n <= 1)
+        return;
+  
+    // Insert last element at its correct position
+    // in sorted array.
+    int last = arr[n-1];
+    int j = n-2;
+ 
+    /* Move elements of arr[0..i-1], that are
+    greater than key, to one position ahead
+    of their current position */
+    while (j >= 0 && arr[j] < last)
+    {
+        arr[j+1] = arr[j];
+        j--;
+    }
+    arr[j+1] = last;
+  
+   insertionSortRecursive( arr, n-1 );
+}
